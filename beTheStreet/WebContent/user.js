e40bb0a66fd9a9773b8bb63961d047bf9c9ee432
duanfa <duanfa@gmail.com>
2014-01-15 22:14:21 +0800

    aaaaa

--- beTheStreet/WebContent/user.js
+++ beTheStreet/WebContent/user.js
@@ -6,16 +6,22 @@ function addItems(){
 	$.get("api/user/list/page?page=1&size=10", function( data ) {}).done(function(data) {
 		var result = "";
 		$(data.result).each(function(index, value) {
+			var statu = "";
+			if(value.active==true){
+				statu = '<span class="label label-success">Active</span>';
+			}else{
+				statu = '<span class="label label-important">Inactive</span>';
+			}
 			var content = '<tr>'+
 				'<td>'+value.name+'</td>'+
-				'<td class="center">'+value.email+'</td>'+
-				'<td class="center">'+value.address+'</td>'+
-				'<td class="center">'+value.cellphone+'</td>'+
-				'<td class="center">'+value.statu+'</td>'+
+				'<td class="center">'+validate(value.email)+'</td>'+
+				'<td class="center">'+validate(value.address)+'</td>'+
+				'<td class="center">'+validate(value.cellphone)+'</td>'+
+				'<td class="center">'+statu+'</td>'+
 				'<td class="center">'+
 					'<a class="btn btn-success" href="userImage.html?userid='+value.id+'"><i class="icon-picture icon-white"></i>Image</a>&nbsp;'+
-					'<a class="btn btn-info edite-user" onclick="updateMsg('+index+')" href="#"> <i class="icon-edit icon-white"></i> Edite </a>&nbsp;'+
-					'<a class="btn btn-danger" onclick="deleteMsg('+this.id+')" href="#"> <i class="icon-trash icon-white"></i> Delete </a>'+
+					/*'<a class="btn btn-info edite-user" onclick="updateMsg('+index+')" href="#"> <i class="icon-edit icon-white"></i> Edite </a>&nbsp;'+
+					'<a class="btn btn-danger" onclick="deleteMsg('+this.id+')" href="#"> <i class="icon-trash icon-white"></i> Delete </a>'+*/
 				'</td>'+
 			'</tr>';
 			result = result+content;
@@ -24,8 +30,11 @@ function addItems(){
 	});
 
 }
-function deleteUser(userId){
-	$.get("api/user/del/"+msgId, function( data ) {}).done(function(data) {
-		addItems();
-	});
+function validate(value){
+	console.log(value);
+	if(value==null||value==undefined){
+		return '';
+	}else{
+		return value;
+	}
 }
