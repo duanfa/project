e0014dbb7a6e2b8f73fac525bc5b4fb871425d19
duanfa <duanfa@gmail.com>
2014-01-22 22:18:06 +0800

    aaaaaaaaaaa

--- beTheStreet/src/stdio/kiteDream/module/feedback/controller/FeedbackController.java
+++ beTheStreet/src/stdio/kiteDream/module/feedback/controller/FeedbackController.java
@@ -1,7 +1,5 @@
 package stdio.kiteDream.module.feedback.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
