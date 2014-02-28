b95ed6e1256f8700acbdda1f90cf37566158ad49
duanfa <duanfa@gmail.com>
2014-02-28 21:08:06 +0800

    aaaaaaaaaaaaaa

--- beTheStreet/src/stdio/kiteDream/module/user/bean/User.java
+++ beTheStreet/src/stdio/kiteDream/module/user/bean/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -67,7 +68,7 @@ public class User implements Serializable {
 	
 	private int high_level_stage;
 	
-	@ElementCollection
+	@ElementCollection(fetch=FetchType.EAGER)
 	@Basic
  	private Map<Integer,Integer> bonusStatu = new TreeMap<Integer,Integer>();
 
