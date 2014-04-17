1137aedd8d9cd0ad1b4253f0ae8d02f0ad4b6399
duanfa <duanfa@gmail.com>
2014-04-17 22:14:20 +0800

    aaaaaaaaaaa

--- beTheStreet/src/stdio/kiteDream/module/message/bean/MessageType.java
+++ beTheStreet/src/stdio/kiteDream/module/message/bean/MessageType.java
@@ -2,7 +2,7 @@ package stdio.kiteDream.module.message.bean;
 
 public enum MessageType {
 
-	CHALLENGE("challenge", 1), CHA_CHING("cha-ching", 2), BROADCAST("BROADCAST", 2);
+	CHALLENGE("challenge", 1), CHA_CHING("cha-ching", 2), BROADCAST("BROADCAST", 3);
 	private String name;
 	private int value;
 	MessageType(String name, int value){
