cabb1995f3b501c414f7f668a9ba72eadb4f00a1
duanfa <duanfa@xinnet.com>
2014-04-22 14:12:50 +0800

    aaaaaaaaaaaaaaa

--- beTheStreet/src/level.sql
+++ beTheStreet/src/level.sql
@@ -1,7 +1,29 @@
-CREATE TABLE level (id  integer, completeNum integer not null, desc varchar, level integer not null, path varchar, thumbnail_path varchar, title varchar, type integer, coinid integer, greenRatio integer not null default 0, redRatio integer not null default 0, sumcoins integer not null default 0, yellowRatio integer not null default 0, bonus_stage integer not null default 0, isChallenge integer not null default 0, regular_stage integer not null default 0, desc2 varchar, title2 varchar, shortdesc varchar, shortdesc2 varchar, primary key (id));
-INSERT INTO "level" VALUES(1, 7, 'Search Street View to find 2 trash cans or recycle bins.', 2, NULL, NULL, 'Level 2', NULL, 18, 10, 80, 10, 10, 1, 0, 2, 'How''s your aim? Send in a photo of trash as it is flying into a trashcan or recycle bin.', 'Level 2 Challenge', 'Find 2 Trash Cans', 'Flying trash photo');
-INSERT INTO "level" VALUES(2, 12, 'Search Street View to locate your favorite hangout spot. Then submit a screenshot.', 1, NULL, NULL, 'Level 1', NULL, 20, 30, 40, 10, 30, 1, 0, 1, 'Snap and send in a photo of friends hangin'' out.', 'Level 1 Challenge', 'Find your hangout spot', 'Photo of hangout spot');
-INSERT INTO "level" VALUES(3, 5, 'Search Street View to find 3 storm drains', 3, NULL, NULL, 'Level 3', NULL, 22, 65, 5, 30, 30, 1, 0, 3, 'Storm drain say what? Send in a photo of you or a friend pointing to the message printed on any Bay Area storm drain.', 'Level 3 Challenge', 'Find Storm Drains', 'Storm drain say what?');
-INSERT INTO "level" VALUES(4, 4, 'Search Street View to find 3 shops that sell either used or reusable stuff', 4, NULL, NULL, 'Level 4', NULL, 23, 90, 1, 10, 9, 1, 0, 2, 'Trash or treasure? Send in a photo of an old object being re-used in a new or different way.', 'Level 4 Challenge!', 'Find shops selling reusables', 'Creative reuse');
-INSERT INTO "level" VALUES(5, 3, 'Search Street View to find a group of 3 or more people working together', 5, NULL, NULL, 'Level 5', NULL, 24, 90, 1, 15, 9, 1, 0, 1, 'Join or create a group and compete against rival groups to earn real life prizes.', 'Level 5 Challenge', 'Find a group', 'Join a Group');
-INSERT INTO "level" VALUES(6, 3, 'Search Street View to locate your favorite hangout spot. Then submit a screenshot.', 6, NULL, NULL, 'title', NULL, NULL, 10, 70, 15, 20, 1, 1, 0, NULL, NULL, '6 short description', NULL);
+DROP TABLE IF EXISTS `level`;
+
+CREATE TABLE `level` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bonus_stage` int(11) NOT NULL,
+  `completeNum` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `desc2` varchar(255) DEFAULT NULL,
+  `greenRatio` int(11) NOT NULL,
+  `isChallenge` tinyint(1) NOT NULL,
+  `level` int(11) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `redRatio` int(11) NOT NULL,
+  `regular_stage` int(11) NOT NULL,
+  `shortdesc` varchar(255) DEFAULT NULL,
+  `shortdesc2` varchar(255) DEFAULT NULL,
+  `sumcoins` int(11) NOT NULL,
+  `thumbnail_path` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `title2` varchar(255) DEFAULT NULL,
+  `yellowRatio` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `level` WRITE;
+/*!40000 ALTER TABLE `level` DISABLE KEYS */;
+INSERT INTO `level` VALUES (1,1,15,'Search Street View to locate your favorite hangout spot. Then submit a screenshot.','Snap and send in a photo of friends hangin\' out.',80,0,1,NULL,5,1,'Hangout Spot','Hangout Photo',5,NULL,'Level 1','Level 1 Challenge',15),(2,1,7,'Search Street View to find 2 trash cans or recycle bins.','How\'s your aim? Send in a photo of trash as it is flying into a trashcan or recycle bin.',80,0,2,NULL,5,2,'Trash Cans','Flying Trash',10,NULL,'Level 2','Level 2 Challenge',15),(3,1,2,'Search Street View to find 3 storm drains','Storm drain say what? Send in a photo of you or a friend pointing to the message printed on any Bay Area storm drain.',80,0,3,NULL,5,3,'Storm Drains','Storm Drain',15,NULL,'Level 3','Level 3 Challenge',15),(4,1,1,'Search Street View to find 3 shops that sell either used or reusable stuff','Trash or treasure? Send in a photo of an old object being re-used in a new or different way.',80,0,4,NULL,5,2,'Re-use Shops','Creative Re-use',20,NULL,'Level 4','Level 4 Challenge',15),(5,1,1,'Search Street View to find a group of 3 or more people working together','Join or create a group and compete against rival groups to earn real life prizes.',80,0,5,NULL,5,1,'Teamwork','Join a Group',25,NULL,'Level 5','Level 5 Challenge',15),(6,1,0,'Do you know what is the single most commonly littered item in the world? Send us a photo of one',NULL,80,1,6,NULL,5,0,'Most Littered',NULL,30,NULL,'Trivia Challenge 1',NULL,15);
+/*!40000 ALTER TABLE `level` ENABLE KEYS */;
+UNLOCK TABLES;
